a43c33eb721639af7f58d93fb7e53d3f349370ba
piraveena <piraveena18@gmail.com>
2023-03-27 11:42:10 +0530

    Add initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# How to run the project
+1. In the terminal, run `mvn clean spring-boot:run`
+2. In the browser, go to `http://localhost:8080/`
---
+++ src/test/java/com/identity/asgardeo/samples/springbootsampleapp/SpringBootSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identity.asgardeo.samples.springbootsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
